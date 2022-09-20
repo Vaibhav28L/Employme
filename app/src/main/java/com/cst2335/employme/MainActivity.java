a69523e28b565a97747af4f6c73263a69c47677d
Vaibhav Vaibhav <vaibhavsharma0933@gmail.com>
2022-09-20 04:44:13 -0400

    little work

---
+++ app/src/main/java/com/cst2335/employme/MainActivity.java
@@ -0,0 +1,23 @@
+package com.cst2335.employme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+    }
+}
